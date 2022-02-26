f9f56f3f1a9836293c4e51ace14722be656556ad
Christian Keil <christian@astranis.com>
2022-02-25 23:02:22 -0800

    commit tree c56199e84f074b1dc610c92b691c5a6a3b5e4619

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function isWin(element) {
+    return (element.result === "W");
+}
+
+function superbowlWin(array) {    
+    if (array.find(isWin) !== undefined) {
+        return array.find(isWin).year
+    } else {return undefined}
+}
